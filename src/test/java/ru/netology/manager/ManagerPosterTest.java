ebae48910931e58db2e67b53b73521372e339439
FilStas <filstas23@gmail.com>
2020-06-10 17:30:30 +0400

    added ManagerPosterTest

---
+++ src/test/java/ru/netology/manager/ManagerPosterTest.java
@@ -0,0 +1,10 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PosterMovies;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerPosterTest {
+    private ManagerPoster managerPoster = new ManagerPoster();
+
+}
