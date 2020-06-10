f4ce097aa6881912f6c53f3c8e0e94dd8701adb2
FilStas <filstas23@gmail.com>
2020-06-10 12:53:03 +0400

    modified PosterMovies

--- src/main/java/ru/netology/domain/PosterMovies.java
+++ src/main/java/ru/netology/domain/PosterMovies.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MoviePoster {
+public class PosterMovies {
     private int id;
     private String imageUrl;
     private String movieName;
